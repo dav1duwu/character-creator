505b887ef4dd8629ce3d2b9cb2facd1d792872e8
David Odstrčil <david.odstrcil@educhem.cz>
2023-04-16 21:54:09 +0200

    Přidejte soubory projektu.

---
+++ chara_crt/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chara_crt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool _dark_mode = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn_tma_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Background == Brushes.White)
+            {
+                this.Background = Brushes.Black;
+            }
+            uwu.Foreground = Brushes.White;
+            uwu.Background = Brushes.Black;
+
+            _dark_mode = true;
+        }
+        private void btn_crt_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1(_dark_mode);
+            window1.Show();            
+        }
+        private void btn_sv_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Background == Brushes.Black)
+            {
+                this.Background = Brushes.White;
+            }
+            uwu.Foreground = Brushes.Black;
+            uwu.Background = Brushes.White;
+
+            _dark_mode = false;
+        }
+        private void btn_load_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ chara_crt/Window1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace chara_crt
+{
+    /// <summary>
+    /// Interakční logika pro Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private bool dark_mode = false;     
+        public Window1(bool _dark_mode)
+        {            
+            InitializeComponent();                        
+            dark_mode = _dark_mode;
+
+            cbx_class.Items.Add("člověk");
+            cbx_class.Items.Add("bestie");
+
+            cbx_class.SelectedIndex = 0;
+
+            if(dark_mode == true)
+            {
+                this.Background = Brushes.Black;
+
+                lol.Foreground = Brushes.White;
+                lol.Background = Brushes.Black;
+
+                lul.Foreground = Brushes.White;
+                lul.Background = Brushes.Black;
+
+                lal.Foreground = Brushes.White;
+                lal.Background = Brushes.Black;
+            }
+        }
+        private void btn_class_mec_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_class_luk_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_class_hul_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_class_horda_Click(object sender, RoutedEventArgs e)
+        {
+            border_color.BorderBrush = new SolidColorBrush(Colors.Red);
+        }
+        private void btn_class_aliance_Click(object sender, RoutedEventArgs e)
+        {
+            border_color.BorderBrush = new SolidColorBrush(Colors.Cyan);
+        }
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
